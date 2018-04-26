2c8fcf8e115036957e79831e5461c87d160adaf6
JohanSonmark <johansonmark@hotmail.com>
2018-04-26 13:44:32 +0200

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {makeMove, newGame} from '../logic';
+
+import Message from './message';
+import Tile from './tile';
+
+/*
+The main game App! It should have a TicTacToe game state in its component state,
+and use the Tile and Message components to render the game.
+
+Then the `makeMove` function from the logic layer should be used to update the
+game state as the tiles are being clicked.
+
+The user should also be able to reset the game.
+
+The App component should render an outer element with a `container` CSS class,
+and all tiles in an element with a `board` CSS class.
+*/
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <div id="game">
+        <div id="head">
+          Spela tre i rad
+        </div>
+        <div id="board" onClick={(e)=>this.clicked(e)}>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+        </div>
+      </div>
+    );
+  }
+}
